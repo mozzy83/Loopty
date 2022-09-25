6b1a59604f2c2c0860c2371d18e3901c656c0bd5
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2022-09-24 19:01:03 -0600

    Build The App page

--- js/site.js
+++ js/site.js
@@ -1,4 +1,45 @@
-//Call Hello World
-function HellowWorld(){
-    alert("Hello World");
+//get the values from UI
+function getValues(){
+
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if(startValue>endValue)alert("Start value must be smaller than end value");
+
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+        let numbers = generateNumbers(startValue, endValue);
+        displayNumbers(numbers);
+    }
+    else{alert("Please enter integers");}
+}
+
+//generate numbers from startValue to the endValue
+    function generateNumbers(startValue, endValue){
+        let numbers = [];
+        for (let index = startValue; index <= endValue; index++) {            
+            numbers.push(index);            
+        }
+        return numbers;
+    }
+
+//display numbers making evens bold
+function displayNumbers(numbers){
+
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+        let className = "";
+        let number = numbers[index];
+
+        if(number % 2 == 0){className="even";}
+        else{className="odd";}
+        
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
+
 }
